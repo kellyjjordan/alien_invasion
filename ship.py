9aa44af693e02a4928e0577857ea8f74f445d596
Kelly Jordan <jordakel@sheridancollege.ca>
2024-12-28 12:01:17 -0500

    added in a settings for ship speed so the speed can change later on in the game

--- ship.py
+++ ship.py
@@ -6,10 +6,15 @@ class Ship:
         #initialize the ship and its starting pos in the game
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #load the ship and its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
+        #store a decimal value for the ships horizontal pos
+        self.x = float(self.rect.x)
+
 
         #start ship at the bottom center 
         self.rect.midbottom = self.screen_rect.midbottom 
@@ -19,9 +24,9 @@ class Ship:
 
     def update(self):
         if self.move_right:
-            self.rect.x+= 1
+            self.rect.x+= self.settings.ship_speed
         if self.move_left:
-            self.rect.x -=1 # - since its going to the negative x value plane 
+            self.rect.x -= self.settings.ship_speed # - since its going to the negative x value plane 
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
