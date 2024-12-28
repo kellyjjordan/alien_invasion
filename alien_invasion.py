e40bb200cd3e141d168b234fc28163cf2e712896
Kelly Jordan <jordakel@sheridancollege.ca>
2024-12-28 12:20:41 -0500

    fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,9 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) #size of window screen
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) #size of window screen
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         
         
