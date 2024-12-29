be4cc4df830a0eddfecef7017bc50311a5e6dfb9
Kelly Jordan <jordakel@sheridancollege.ca>
2024-12-29 15:44:47 -0500

    created bullet class and its settings

---
+++ bullet.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    #class to manage bullets fried from shio
+
+    def __init__(self, ai_game):
+        super().__init__() #inherits from sprite class
+
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings 
+        self.colour = self.settings.bullet_colour
+
+        #creating a buillet rect at 0,0 and then set correct pos 
+        self.rect = pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #store the bullets pos as a deciaml value
+        self.y = float(self.rect.y)
+
+        #using update function to manage the bullets position
+    def update(self):
+        #moving bullet up the screen
+        self.y -=self.settings.bullet_speed
+        self.rect.y = self.y #updating the rect position
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen,self.colour,self.rect)
--- setting.py
+++ setting.py
@@ -9,3 +9,9 @@ class Settings:
 
         #ship settings
         self.ship_speed = 1.5
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.buleet_height = 15
+        self.bullet_colour = (60,60,60)
